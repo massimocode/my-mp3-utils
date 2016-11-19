b3c9dfda39d10c7d1f705fa8055335566b1fc60b
Massimo Hamilton <massimo.hamilton@outlook.com>
2016-11-19 18:43:43 +0000

    Initial commit

---
+++ MyMp3Utils/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using TagLib;
+
+namespace MyMp3Utils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mp3Paths = Directory.GetFiles(args[0], "*.mp3", SearchOption.AllDirectories);
+            foreach (var mp3 in mp3Paths)
+            {
+                var file = TagLib.File.Create(mp3);
+                file.Tag.Pictures = new IPicture[0];
+                file.Tag.Track = 0;
+                file.Tag.TrackCount = 0;
+                file.Tag.Year = 0;
+                file.Save();
+            }
+        }
+    }
+}
